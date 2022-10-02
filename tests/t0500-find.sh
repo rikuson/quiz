f9dc4a97511eec15e817bd510055037488e0da1b
Riku Takeuchi <rikuson@users.noreply.github.com>
2022-10-02 13:57:30 +0900

    Remove generate subcommand

--- tests/t0500-find.sh
+++ tests/t0500-find.sh
@@ -6,12 +6,12 @@ cd "$(dirname "$0")"
 
 test_expect_success 'Make sure find resolves correct files' '
 	"$QUIZ" init $KEY1 &&
-	"$QUIZ" generate Something/neat 19 &&
-	"$QUIZ" generate Anotherthing/okay 38 &&
-	"$QUIZ" generate Fish 12 &&
-	"$QUIZ" generate Fishthings 122 &&
-	"$QUIZ" generate Fishies/stuff 21 &&
-	"$QUIZ" generate Fishies/otherstuff 1234 &&
+	"$QUIZ" insert Something/neat -e <<<"$(fake-answer.sh 19)" &&
+	"$QUIZ" insert Anotherthing/okay -e <<<"$(fake-answer.sh 38)" &&
+	"$QUIZ" insert Fish -e <<<"$(fake-answer.sh 12)" &&
+	"$QUIZ" insert Fishthings -e <<<"$(fake-answer.sh 122)" &&
+	"$QUIZ" insert Fishies/stuff -e <<<"$(fake-answer.sh 21)" &&
+	"$QUIZ" insert Fishies/otherstuff -e <<<"$(fake-answer.sh 1234)" &&
 	[[ $("$QUIZ" find fish | sed "s/^[ \`|-]*//g;s/$(printf \\x1b)\\[[0-9;]*[a-zA-Z]//g" | tr "\\n" -) == "Search Terms: fish-Fish-Fishies-otherstuff-stuff-Fishthings-" ]]
 '
 
